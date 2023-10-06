0b9a0ed7e9e37e84bcfa9f7f7bf4d9644dbbd3c2
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2023-10-06 13:06:29 +0300

    restructuring

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,15 @@
+#!/usr/bin/python3
+
 def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened.
+    :param list[list] boxes: List of lists, where each sub-list is a row in the box and contains 0s or
+    Args:
+        boxes (list of lists): A list of locked boxes. Each box is represented as a list of keys.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
     n = len(boxes)
     visited = [False] * n
     stack = [0]  # Start with the first box, which is already unlocked
@@ -13,12 +24,13 @@ def canUnlockAll(boxes):
 
     return all(visited)
 
-# Test cases
-boxes1 = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes1))  # True
+if __name__ == "__main__":
+    # Test cases
+    boxes1 = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes1))  # True
 
-boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes2))  # True
+    boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes2))  # True
 
-boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes3))  # False
+    boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes3))  # False
